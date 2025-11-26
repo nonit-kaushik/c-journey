68f91b548ec7c855e9f84513e8b1403e88339162
nonit-kaushik <tusharsharma1328@gmail.com>
2025-11-26 13:02:49 +0530

    TH sorting method

--- 03_C_homework/1d_array/15_address.c
+++ 03_C_homework/1d_array/15_address.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    int arr1[5]={1,2,3,4,5,6,7,8,9,10};
-    
+int main() {
+    int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+    int *a = &arr1[0];   // pointer
+
+    int n;
+    printf("Enter the index of element whose address is to be found: ");
+    scanf("%d", &n);
+
+    int *address = a + n;  // C handles size automatically
+
+    printf("The address is %p\n", address);
+
     return 0;
 }
