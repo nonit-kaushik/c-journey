9025cd971df1f81682e2d89ce8ad6dce344411f9
nonit-kaushik <tusharsharma1328@gmail.com>
2025-12-14 10:05:40 +0530

    3d array elements address printing and visualisation

---
+++ 01_C_Language/Chapter 07/Practice/08_problem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int arr[2][2][2] = {1,2,3,4,5,6,7,8};
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                printf("%u ",&arr[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
